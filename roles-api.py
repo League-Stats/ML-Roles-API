a65210fde00b5488b24e6d8d5832b251e9e023bb
JaySebagh <jaysebagh@gmail.com>
2020-06-05 18:42:59 -0700

    updated py

--- roles-api.py
+++ roles-api.py
@@ -21,4 +21,6 @@ def roles():
     prediction = roleml.predict(match, timeline)
     return prediction
 
-app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
