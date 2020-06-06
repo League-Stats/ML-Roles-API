dca92d5f695ca910a1a9bd80635617781b8e253e
JaySebagh <jaysebagh@gmail.com>
2020-06-05 18:04:06 -0700

    successfully passed data to MLRoles and returned the roles

---
+++ roles-api.py
@@ -0,0 +1,24 @@
+import flask
+import json
+from flask import request, jsonify
+import roleml
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Python API</h1><p>API home endpoint is functional.</p>"
+
+@app.route('/api/roleml', methods=['POST'])
+def roles():
+    req_data = request.get_json()
+    getMatch = req_data['match']
+    getTimeline = req_data['timeline']
+    match = json.loads(getMatch)
+    timeline = json.loads(getTimeline)
+
+    prediction = roleml.predict(match, timeline)
+    return prediction
+
+app.run()
